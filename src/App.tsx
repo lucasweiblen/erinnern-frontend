024240d60a902af63ba2ba4d3e538adf3d42a89c
Lucas Weiblen <lucasweiblen@gmail.com>
2019-06-09 14:49:29 -0300

    extract urls

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,7 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-
-const urls = [
-  {
-    id: 1,
-    url: 'http://www.foobar.com',
-    tags: ['foo', 'bar'],
-  },
-  {
-    id: 2,
-    url: 'http://www.trendingrepos.xyz',
-    tags: ['uol', 'netflix'],
-  },
-  {
-    id: 3,
-    url: 'http://www.blablacar.com',
-    tags: ['react', 'hooks'],
-  },
-];
+import urls from './urls';
 
 interface Url {
   id: number;
---
+++ src/urls.ts
@@ -0,0 +1,19 @@
+const urls = [
+  {
+    id: 1,
+    url: 'http://www.foobar.com',
+    tags: ['foo', 'bar'],
+  },
+  {
+    id: 2,
+    url: 'http://www.trendingrepos.xyz',
+    tags: ['uol', 'netflix'],
+  },
+  {
+    id: 3,
+    url: 'http://www.blablacar.com',
+    tags: ['react', 'hooks'],
+  },
+];
+
+export default urls;
